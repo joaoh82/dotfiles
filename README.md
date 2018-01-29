8ad4fe25872138e7efa81f8652d7ad979d16c84e
João Henrique Machado Silva <joaoh82@gmail.com>
2018-01-29 16:00:56 +0100

    added .gitcookies file and install.sh

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # dotfiles
-My main dot config files.
+My main dot config files, and instructions to setup a new mac.
+
+Steps to setup new computer.
+OS.: OSX
+
+
---
+++ install.sh
@@ -0,0 +1,9 @@
+cd /usr/locall
+mkdir w && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C homebrew
+brew update
+brew install fish
+brew install git
+brew install protobuf
+brew install ethereum
+brew tap ethereum/ethereum
+brew install ethereum/ethereum
